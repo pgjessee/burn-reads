db97048119b951faa4a139db24a898f267fa9f4b
pgjessee <peter.jessee@gmail.com>
2021-01-30 13:10:44 -0600

    error handlers on backend are now in place

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -9,17 +9,3 @@ router.post('/test', (req, res) => {
 
 module.exports = router;
 
-(async () => {
-    let res = await fetch('/api/test', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "XSRF-TOKEN": `uif7JARb-Mkn8hufNkXQ5iTOiowtyQ8qqA2A`
-        },
-        body: JSON.stringify({ hello: 'world' })
-    })
-
-    let data = await res.json();
-    console.log(data);
-    return data;
-})()
