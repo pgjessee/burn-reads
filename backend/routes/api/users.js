82c5de79341cd6abb3bd6c6adb863805e7da5d5f
pgjessee <peter.jessee@gmail.com>
2021-01-30 18:53:18 -0600

    user signup route testing success

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 
+const { setTokenCookie } = require('../../utils/auth')
+const { User } = require('../../db/models')
+
 const router = express.Router();
 
 
 router.post('/', asyncHandler(async(req, res) => {
-    
+    const { first_name, email, zip, password} = req.body;
+
+    const user = await User.signup({
+        first_name,
+        email,
+        zip,
+        password
+    })
+
+    setTokenCookie(res, user);
+
+    return res.json({ user })
+
 }))
 
 
 module.exports = router;
+
+
+
