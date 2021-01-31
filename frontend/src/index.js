56cdc7578c38de2553e459d085f4ae41b598276f
pgjessee <peter.jessee@gmail.com>
2021-01-30 21:57:04 -0600

    frontend setup is complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,13 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import configureStore from './store/index';
+import { restoreCSRF, fetch} from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+  window.csrfFetch = fetch;
   window.store = store;
 };
 
