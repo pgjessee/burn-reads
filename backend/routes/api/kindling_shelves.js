791b52d2f0ebed533dc8183fc1636a30d84ce541
ezpinsky <ezpinsky@gmail.com>
2021-02-04 12:14:26 -0500

    adds default shelves to new user upon signup

--- backend/routes/api/kindling_shelves.js
+++ backend/routes/api/kindling_shelves.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 
-const { Kindling_Shelf } = require('../../db/models')
+const { Kindling_Shelf } = require('../../db/models');
 
 const router = express.Router();
+const defaultShelfNames = ['Torched', 'Torching', 'Want to Torch'];
 
+router.post(
+	'/new-user',
+	asyncHandler(async (req, res, next) => {
+		const defaultShelves = {};
+		const { user_id } = req.body;
+		defaultShelfNames.forEach(async shelf_name => {
+			const newShelf = await Kindling_Shelf.create({ shelf_name, user_id });
+			defaultShelves[shelf_name] = newShelf;
+		});
+		return res.json(defaultShelves);
+	})
+);
 
-router.post("/new-user", asyncHandler(async(req, res, next) => {
-    const { shelf_name, user_id } = req.body;
-
-    const newShelf = await Kindling_Shelf.create({
-        shelf_name,
-        user_id
-    })
-
-    return res.json({ newShelf })
-}))
-
+router.post(
+	'/shelves',
+	asyncHandler(async (req, res, next) => {
+		const { shelf_name, user_id } = req.body;
+		const newShelf = await Kindling_Shelf.create({
+			shelf_name,
+			user_id,
+		});
+		return res.json({ newShelf });
+	})
+);
 
 module.exports = router;
