00ff0b941517a76ce439af06c9c4b7016ed78519
ezpinsky <ezpinsky@gmail.com>
2021-02-04 10:50:07 -0500

    adds port specification for docker postgres

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,19 +5,21 @@ const username = db.username;
 const password = db.password;
 const database = db.database;
 const host = db.host;
+const port = db.port;
 
 module.exports = {
-  development: {
-    username,
-    password,
-    database,
-    host,
-    dialect: 'postgres',
-    seederStorage: "sequelize",
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-  },
+	development: {
+		username,
+		password,
+		database,
+		host,
+		port,
+		dialect: 'postgres',
+		seederStorage: 'sequelize',
+	},
+	production: {
+		use_env_variable: 'DATABASE_URL',
+		dialect: 'postgres',
+		seederStorage: 'sequelize',
+	},
 };
