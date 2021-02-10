da714d748109956a6016811aad7af97da5091824
ezpinsky <ezpinsky@gmail.com>
2021-02-10 11:57:35 -0500

    search results test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import { useDispatch } from 'react-redux';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignUpFormPage from './components/SignupFormPage/SignUpFormPage';
-import BookProfilePage from './components/BookProfilePage'
-import WriteReviewPage from './components/WriteReviewPage'
+import BookProfilePage from './components/BookProfilePage';
+import WriteReviewPage from './components/WriteReviewPage';
+import SearchResults from './components/SearchResults';
 import Test from './components/Test';
 import * as sessionActions from './store/session';
 
@@ -25,14 +26,17 @@ function App() {
 					<Route path='/signup'>
 						<SignUpFormPage />
 					</Route>
-					<Route path='/:googleBookId' exact={true} >
+					<Route path='/:googleBookId' exact={true}>
 						<BookProfilePage />
 					</Route>
-					<Route path='/:googleBookId/reviews' exact={true} >
+					<Route path='/:googleBookId/reviews' exact={true}>
 						<WriteReviewPage />
 					</Route>
 					<Route path='/'>
 						<Test />
+					</Route>
+					<Route path='/search-results'>
+						<SearchResults />
 					</Route>
 				</Switch>
 			</BrowserRouter>
